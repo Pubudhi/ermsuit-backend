1922099f7350424604082006bb3aee1f1268ec29
pubudhi <pywitharana@gmail.com>
2025-06-25 00:34:21 +0530

    Initial commit: spring backend set up

---
+++ src/main/java/com/example/ermsuitbackend/ErmsuitBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.ermsuitbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErmsuitBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErmsuitBackendApplication.class, args);
+	}
+
+}
